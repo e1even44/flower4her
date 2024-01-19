0b5b818f2a4c93f3b9b00cbe098cf3b545ca0cd1
Hilal Karsli <hilalkarsli@icloud.com>
2024-01-19 10:37:17 +0100

    added new color palette

---
+++ pinkFlower.py
@@ -0,0 +1,29 @@
+from turtle import *
+import colorsys
+
+# settings
+bgcolor("black")
+tracer(2)
+pensize(2)
+h = 0.6
+scale_facor = 0.25
+
+for i in range(250):
+    color = colorsys.hsv_to_rgb(h, 0.5, 1)
+    h += 0.0015
+    pencolor(color)
+
+    # scaling coordinates manually
+    x, y = pos()
+    x *= scale_facor
+    y *= scale_facor
+    goto(x, y)
+
+    lt(179)
+    backward(i * 0.1 * scale_facor)
+    circle(i * 3 * scale_facor, 120)
+    rt(14)
+    forward(i * 0.1 * scale_facor)
+    circle(i * 0.3 * scale_facor, 120)
+
+done()
